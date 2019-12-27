1db1a2d19369697467f1947e2c7b72176e833427
KTO <toklow@poczta.onet.pl>
2019-12-27 11:20:59 +0100

    Product & User classes + tests

--- src/main/java/impl/ProductServiceImpl.java
+++ src/main/java/impl/ProductServiceImpl.java
@@ -1,7 +1,6 @@
 package impl;
 
 import entity.Product;
-import entity.User;
 import iface.ProductService;
 
 import java.util.ArrayList;
@@ -25,19 +24,52 @@ public class ProductServiceImpl implements ProductService  {
         return this.products;
     }
 
+    // It may happen that we have 2 products with the same name, so function should return a list of found products
     public Product getProductByName(String productName) {
+
+        if (this.products.isEmpty()) { return null; }
+
+        for (Product product : this.products) {
+            if (product.getProductName().equals(productName)) {
+                return product;
+            }
+        }
         return null;
     }
 
     public boolean isProductOnStockByName(String productName) {
+        for (Product product : this.products)
+        {
+            // It may happen that we have 2 products with the same name.
+            // That's why the loop should be interrupted if we found the product and the stock is not empty for it
+            if (product.getProductName().equals(productName) && product.getProductCount() > 0.0)
+            {
+               return true;
+            }
+        }
         return false;
     }
 
     public boolean doesProductExistByName(String productName) {
+        for (Product product : this.products)
+        {
+            if (product.getProductName().equals(productName))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean doesProductExistById(Long productId){
+    public boolean doesProductExistById(Long productId)
+    {
+        for (Product product : this.products)
+        {
+            if (product.getId().equals(productId))
+            {
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/test/java/servicetest/ProductServiceTest.java
+++ src/test/java/servicetest/ProductServiceTest.java
@@ -13,21 +13,154 @@ public class ProductServiceTest {
     @Test
     public void testGetAllProducts() {
         //is
-        Product marvel = new Product(100L, "Marvel", 200.75, 10.0, "Brown", 100.0);
-        Product shirt = new Product(200L, "Shirt", 300.0, 0.4, "White", 5.0);
-        Product sneakers = new Product(300L, "Sneakers", 750.0, 0.8, "Brown", 15.0);
-
-        List<Product> products = new ArrayList<Product>();
-        products.add(marvel);
-        products.add(shirt);
-        products.add(sneakers);
+        List<Product> products = createProductsForTests();
 
         //then
         ProductServiceImpl productService = new ProductServiceImpl(products);
         List<Product> productsFromTestClass = productService.getAllProducts();
 
         //expected
-        Assertions.assertEquals(products, productsFromTestClass);   
-        
+        Assertions.assertEquals(products, productsFromTestClass);
+    }
+
+    @Test
+    public void testGetAllProductsNegative() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        List<Product> productsFromTestClass = productService.getAllProducts();
+        products.remove(1);
+
+        //expected
+        Assertions.assertNotEquals(products, productsFromTestClass);
+    }
+
+    @Test
+    public void testGetProductByName() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        Product productFromTestClass = productService.getProductByName(products.get(1).getProductName());
+
+        //expected
+        Assertions.assertNotNull(productFromTestClass);
+    }
+
+    @Test
+    public void testGetProductByNameNegative() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        Product productFromTestClass = productService.getProductByName("Not existing product");
+
+        //expected
+        Assertions.assertNull(productFromTestClass);
+    }
+
+    @Test
+    public void testIsProductOnStockByName() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean isOnStock = productService.isProductOnStockByName(products.get(1).getProductName());
+
+        //expected
+        Assertions.assertTrue(isOnStock);
+    }
+
+    @Test
+    public void testIsProductOnStockByNameNegative1() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean isOnStock = productService.isProductOnStockByName(products.get(2).getProductName()); // Coat - not on stock
+
+        //expected
+        Assertions.assertFalse(isOnStock);
+    }
+
+    @Test
+    public void testIsProductOnStockByNameNegative2() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean isOnStock = productService.isProductOnStockByName("Not existing product");
+
+        //expected
+        Assertions.assertFalse(isOnStock);
+    }
+
+    @Test
+    public void testDoesProductExistByName() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean doesProductExist = productService.doesProductExistByName(products.get(2).getProductName());
+
+        //expected
+        Assertions.assertTrue(doesProductExist);
+    }
+
+    @Test
+    public void testDoesProductExistByNameNegative() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean doesProductExist = productService.doesProductExistByName("Not existing product");
+
+        //expected
+        Assertions.assertFalse(doesProductExist);
+    }
+
+    @Test
+    public void testDoesProductExistById() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean doesProductExist = productService.doesProductExistById(products.get(2).getId());
+
+        //expected
+        Assertions.assertTrue(doesProductExist);
+    }
+
+    @Test
+    public void testDoesProductExistByIdNegative() {
+        //is
+        List<Product> products = createProductsForTests();
+
+        //then
+        ProductServiceImpl productService = new ProductServiceImpl(products);
+        boolean doesProductExist = productService.doesProductExistById(220L);
+
+        //expected
+        Assertions.assertFalse(doesProductExist);
+    }
+
+    private List<Product> createProductsForTests () {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(100L, "Marvel", 200.75, 10.0, "Brown", 100.0));
+        products.add(new Product(200L, "Shirt", 300.0, 0.4, "White", 5.0));
+        products.add(new Product(210L, "Coat", 300.0, 0.4, "White", 0.0));
+        products.add(new Product(300L, "Sneakers", 750.0, 0.8, "Brown", 15.0));
+
+        return products;
     }
 }
